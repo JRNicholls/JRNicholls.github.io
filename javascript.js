9c4f5969d793a46fd5ed0c3a32d71f19b61fa184
James Nicholls <jimyjack@bigpond.net.au>
2018-02-14 15:10:09 +1100

    added grid layout

--- javascript.js
+++ javascript.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
   $("#header-placeholder").load("header.html");
-  $("#demo").html("Hello, World!");
-});
-
-$(document).ready(function() {
-    $.ajax({
-        url : "header.txt",
-        dataType: "html",
-        success : function (data) {
-            $("#header-placeholder").html(data);
-        }
-    });
 });
